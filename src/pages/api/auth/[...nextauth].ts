e0288ee3ff33b5455bd40d931f81651722fa71cf
Oussama Moutafatin <moutafatin3@gmail.com>
2022-06-08 15:56:57 +0100

    add NextAuth

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,14 @@
+import { prisma } from "@/server/prisma";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+});
